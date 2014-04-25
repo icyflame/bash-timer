ff1fa2236760c095f41128e1931db37409072195
icyflame <kannan.siddharth12@gmail.com>
2014-04-25 14:16:38 +0530

    Added a time!

--- timer.sh
+++ timer.sh
@@ -1,4 +1,8 @@
+echo
 echo "Cube timer"
+echo
+
+############# timer sequence #####################
 
 read -p "Enter a key to start timer: " -n 1 -s
 
@@ -7,8 +11,6 @@ echo "Timer running!"
 
 startTime=`date +"%S.%N"`
 
-#read subOpt
-
 read -n 1 -s
 
 endTime=`date +"%S.%N"`
@@ -16,6 +18,8 @@ endTime=`date +"%S.%N"`
 t=`expr $endTime-$startTime | bc`
 
 echo "Time elapsed:- $t"
+
+############## comment sequence ##########################
 
 echo -n "Do you want to add a comment to this time?(y/N) "
 read -n 1 opt
@@ -31,11 +35,13 @@ read comment
 # echo "You entered $comment"
 ;;
 
+*)
+
+echo "You choosed to enter no comment!" ;;
+
 esac
 
-echo "You choosed to enter no comment!"
-
-# we now have the time in the variable t
+########################### write sequence ##########################
 
 if [ "$comment" == "none" ]
 
@@ -46,7 +52,22 @@ else
 echo -n "$t[$comment], " >> times.txt
 fi
 
+################ add and commit sequence #########################
+
 git commit -a -m "Added a time!"
+
+
+###################### push sequence #######################
+
+read -p "Do you want to push all the times currently stored to the server?(y/N): " -n 1 opt
+
+case $opt in
+
+"y" | "Y")
+echo "We will now enter the push sequence"
 git push origin master
+;;
+
+esac
 
 exit 0
