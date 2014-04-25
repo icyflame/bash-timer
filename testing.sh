a8d6714e537890c6beb68f15fefe77366a720485
icyflame <kannan.siddharth12@gmail.com>
2014-04-25 13:32:25 +0530

    Added the script in its most preliminary form! No text file yet started

---
+++ testing.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#echo -n "ENter a key to proceed:"
+#read -n 1
+
+read -p "Enter a key to proceed: " -N 1 -s
---
+++ timer.sh
@@ -0,0 +1,30 @@
+echo "Cube timer"
+
+#echo "Enter a key to start timer: "
+
+# read subOpt
+
+read -p "Enter a key to start timer: " -n 1 -s
+
+echo "Timer running!"
+
+startTime=`date +"%S.%N"`
+
+#read subOpt
+
+read -n 1 -s
+
+endTime=`date +"%S.%N"`
+
+t=`expr $endTime-$startTime | bc`
+
+echo "Time elapsed:- $t"
+
+# we now have the time in the variable t
+
+echo "$t" >> times.txt
+
+git commit -a -m "Added a time!"
+git push origin master
+
+exit 0
