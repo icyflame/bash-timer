8ba7f2e9edc00602f86d6a72f71495394ccb1b65
icyflame <kannan.siddharth12@gmail.com>
2014-04-25 13:45:44 +0530

    Added a time!

--- timer.sh
+++ timer.sh
@@ -20,13 +20,15 @@ echo "Time elapsed:- $t"
 echo -n "Do you want to add a comment to this time?(y/N) "
 read -n 1 opt
 
+comment = "none"
+
 case $opt in
 
 "y" | "Y")
 
 echo "Enter your comment: "
 read comment
-echo "You entered $comment"
+# echo "You entered $comment"
 ;;
 
 esac
@@ -35,7 +37,14 @@ echo "You choosed to enter no comment!"
 
 # we now have the time in the variable t
 
-echo "$t, " >> times.txt
+if [ "$comment" == "none" ]
+
+then
+echo -n "$t, " >> times.txt
+
+else
+echo -n "$t[$comment], " >> times.txt
+fi
 
 git commit -a -m "Added a time!"
 git push origin master
